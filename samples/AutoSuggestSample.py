b246ad8b719e01b08b0d34b9bb76520154b04f9f
Javed Ahmad <v-jaahma@microsoft.com>
2020-04-22 22:12:40 +0530

    refactored Code

--- samples/AutoSuggestSample.py
+++ samples/AutoSuggestSample.py
@@ -1,9 +1,12 @@
+import requests
+import json
+
 subscription_key = 'YOUR-SUBSCRIPTION-KEY'
 tenantId='tenantID'
 indexId='indexId'
 instanceId='BlackFridaySettings'
 
-def RequestAsync():
+def Request():
     search_url = 'https://commerce.bing.com/api/autosuggest/v1/{0}/indexes/{1}/?q=Contoso&count=2&source=logs'
     url=search_url.format(tenantId,indexId)
     headers = {'Authorization':'Bearer '+ subscription_key}
@@ -11,7 +14,7 @@ def RequestAsync():
     response.raise_for_status()
     search_results = response.json()
 
-def RequestIndexAsync():
+def RequestIndex():
     search_url = 'https://commerce.bing.com/api/autosuggestauthoring/v1/{0}/indexes/{1}'
     url=search_url.format(tenantId,indexId)
     headers = {'Authorization':'Bearer '+ subscription_key}
@@ -19,6 +22,8 @@ def RequestIndexAsync():
     response.raise_for_status()
     search_results = response.json()
 
+#Sending a GET request to the mentioned URL should retrieve the main fields of the index if it exists,
+#otherwise it returns a Bad Request error.      
 def GetIndexAsync():
     search_url = 'https://commerce.bing.com/api/autosuggestauthoring/v1/{0}/indexes/{1}'
     url=search_url.format(tenantId,indexId)
@@ -27,6 +32,11 @@ def GetIndexAsync():
     response.raise_for_status()
     search_results = response.json()
 
+#PUT requests are used for both adding a new index and updating an existing one. 
+#The indexId and tenantId are specified in the URL,
+#whereas the body must be of JSON type with two optional fields specifying 
+#whether the autosuggest service is enabled for this index, 
+#and what fields can be used as custom filters.     
 def Add_UpdateIndex():
     search_url = 'https://commerce.bing.com/api/autosuggestauthoring/v1/{0}/indexes/{1}'
     url=search_url.format(tenantId,indexId)
@@ -36,6 +46,7 @@ def Add_UpdateIndex():
     response=requests.put(url,body,headers=headers)
     response.raise_for_status()
 
+#An index can be deleted by sending a DELETE request to the same URL specifying the tenantId and indexId.      
 def DeleteIndex():
     search_url = 'https://commerce.bing.com/api/autosuggestauthoring/v1/{0}/indexes/{1}'
     url=search_url.format(tenantId,indexId)
@@ -44,6 +55,7 @@ def DeleteIndex():
     response.raise_for_status()
     search_results = response.json()
 
+#A GET request is used to retrieve all the blocked suggestions for the indexId given in the URL.       
 def GetBlockedSuggestions():
     search_url = 'https://commerce.bing.com/api/autosuggestauthoring/v1/{0}/indexes/{1}/blocking'
     url=search_url.format(tenantId,indexId)
@@ -51,7 +63,9 @@ def GetBlockedSuggestions():
     response = requests.get(url, headers=headers)
     response.raise_for_status()
 
- 
+#POST requests are used to add blocked suggestions for the specified index.
+#Multiple blocked suggestions can be added on request. Once they are added,
+#each blocked suggestion gets a unique id that can be used for updating or deleting.     
 def CreateBlockedSuggestionsAsync():
     suggs=[]
     p1={'query':'example','matchingType':'Exact'}
@@ -66,6 +80,10 @@ def CreateBlockedSuggestionsAsync():
     addBlockedSuggestionsResponse = requests.post(url, json_data, headers=headers)
     addBlockedSuggestionsResponse.raise_for_status()
 
+#A PUT request is used to update possibly multiple blocked suggestions for the indexId specified in the URL. 
+#The body of the request is of JSON type and contains a list of blocked suggestions 
+#similar to the body of adding new blocked suggestions,
+#with one additional field in each item, which is the id of the blocked suggestion.
 def UpdateBlockdSuggestionsAsync():
     suggs=[]
     p1={'id':'85e1d36d-c044-408e-9bc4-1696c3460f57','query':'blocked suggestion','matchingType':'Contains'}
