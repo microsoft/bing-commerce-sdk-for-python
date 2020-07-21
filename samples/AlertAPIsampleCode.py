1a9ebdd88564cc57197ad6c45a845ea42ea2e4e6
Javed Ahmad <v-jaahma@microsoft.com>
2020-07-21 23:19:08 +0530

    updated code for refresh token API

--- samples/AlertAPIsampleCode.py
+++ samples/AlertAPIsampleCode.py
@@ -80,7 +80,7 @@ def DeleteAlert():
 def RefreshTokenAPIRequest():
     search_url = 'https://commerce.bing.com/api/admin/v1/tenants/{0}/refresh-token'
     url=search_url.format(tenantId)
-    headers = {'Authorization':'Bearer '+ subscription_key,'Content-type':'application/json'}
+    headers = {'Authorization':'Bearer '+ subscription_key,'Content-type':'application/x-www-form-urlencoded'}
     request = {'refresh_token': 'REFRESH TOKEN'}
     json_data = json.dumps(request)
     response=requests.put(url, data=json_data, headers= headers)
